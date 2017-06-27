8abd700457ead77ded9c505711072e55cdef560e
krekel <breakingloco15@gmail.com>
2017-06-27 12:38:51 -0400

    Hide message def

--- steganography.py
+++ steganography.py
@@ -2,25 +2,39 @@
 
 def hide_message(message, image):
 
-    ascii_values = [str(len(message))]
+    # Convert message to ascii
+    ascii_message = [x for x in str(len(message)).zfill(3)]
     for x in message:
-        ascii_values.append(str(ord(x)).zfill(3))
+        for y in str(ord(x)).zfill(3):
+            ascii_message.append(y)
+
+    print(ascii_message)
 
     try:
         with open(image, "r") as f:
-            data = f.readlines()
+            data = f.readlines()[3:]
 
-        # for line in data:
-        #     print(line.replace(b"\n", b""))
-        #
-        # for x in ascii_values:
-        #
-        #
-        # for x in range(3, len(data)):
-        #     for y in range(len(data[x])):
-        #         data[x][y] = [len(data[x][y]) - 1:] +
+        # Image data (no header information)
+        # Store each line of data in a 2d array for better access.
+        # Last value of each line is \n
+        image_data = [line.split(" ") for line in data]
 
-        print(data)
+        # Insert message
+        z = 0
+        for x in range(len(image_data)):
+            for y in range(len(image_data[x])):
+                while z < len(ascii_message):
+                    if image_data[x][y] != "\n":
+                        image_data[x][y] = image_data[x][y][0:len(image_data[x][y]) - 1] + ascii_message[z]
+                        z += 1
+                    break
+
+        # print(data[0])
+        # print(data[1])
+        #
+        # print(image_data[0])
+        # print(image_data[1])
+
 
 
     except FileNotFoundError as e:
