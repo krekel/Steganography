603d99dabce6ae6dad24657791f1f48c8a336bbb
krekel <breakingloco15@gmail.com>
2017-06-27 20:58:55 -0400

    Completed def hide_message()

--- steganography.py
+++ steganography.py
@@ -1,6 +1,14 @@
 
 
+# TODO size restrictions - message can't be greater than image
 def hide_message(message, image):
+
+    """Hide a string inside a ppm image.
+
+    :param message: string to be inserted into the image
+    :param image: ppm image
+    :return:
+    """
 
     # Convert message to ascii
     ascii_message = [x for x in str(len(message)).zfill(3)]
@@ -8,10 +16,10 @@ def hide_message(message, image):
         for y in str(ord(x)).zfill(3):
             ascii_message.append(y)
 
-    print(ascii_message)
-
     try:
         with open(image, "r") as f:
+            header = f.readlines()[0:3]
+            f.seek(0)
             data = f.readlines()[3:]
 
         # Image data (no header information)
@@ -29,18 +37,59 @@ def hide_message(message, image):
                         z += 1
                     break
 
-        # print(data[0])
-        # print(data[1])
-        #
-        # print(image_data[0])
-        # print(image_data[1])
+        # Add header information to image data
+        stego_image = ""
+        for elem in header:
+            stego_image += elem
 
+        for x in range(len(image_data)):
+            for y in range(len(image_data[x])):
+                stego_image += image_data[x][y] + " "
 
+        # Create stego image
+        with open("stego_"+image, 'w') as f:
+            f.write(stego_image)
 
     except FileNotFoundError as e:
         print(e.strerror)
 
 
+def get_message(image):
+
+    try:
+        with open(image, "r") as f:
+            data = f.readlines()[3:]
+
+        # Image data (no header information)
+        # Store each line of data in a 2d array for better access.
+        # Last value of each line is \n
+        image_data = [line.split(" ") for line in data]
+
+        # Get message length
+        length = ""
+        for x in range(3):
+            length += image_data[0][x]
 
 
-hide_message("hello", "castillo.ppm")
+
+        # Insert message
+        z = 0
+        for x in range(len(image_data)):
+            for y in range(len(image_data[x])):
+                while z < len(ascii_message):
+                    if image_data[x][y] != "\n":
+                        image_data[x][y] = image_data[x][y][0:len(image_data[x][y]) - 1] + ascii_message[z]
+                        z += 1
+                    break
+
+        # print(data[0])
+        # print(data[1])
+        #
+        # print(image_data[0])
+        # print(image_data[1])
+        # print(image_data[2])
+    except FileNotFoundError as e:
+        print(e.strerror)
+
+
+hide_message("hello my name is", "castillo.ppm")
