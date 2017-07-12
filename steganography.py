eefc4635d8083b518c31d761c8da7a23a96d84ce
krekel <breakingloco15@gmail.com>
2017-07-12 12:34:29 -0400

    Final Commit

--- steganography.py
+++ steganography.py
@@ -1,4 +1,3 @@
-import multiprocessing
 _magic_number = 'P3'
 
 
@@ -6,7 +5,6 @@ def char_to_bin(string):
     bin_string = [bin(ord(char))[2:].zfill(8) for char in string]
     # Qty of bits for removal purposes pos:1
     bin_string.insert(0, bin((len(bin_string)) * 8)[2:].zfill(8))
-    print(bin_string[0])
 
     # length of bits length number
     fill = len(bin_string[0])
@@ -23,13 +21,10 @@ def ascii_to_bin(data):
 
     for elem in range(2, len(data)):
         bin_data.append(bin(int(data[elem]))[2:].zfill(8))
-    # Qty of bits for removal purposes
-    bin_data.insert(0, bin((len(bin_data)) * 8)[2:].zfill(8))
 
-    # length of bits length number
-    fill = len(bin_data[0])
-    length = len(bin_data[0][2:].zfill(fill))
-    bin_data.insert(0, bin(length)[2:].zfill(8))
+    # Number of bits to be inserted for removal purposes
+    bin_data.insert(0, bin((len(bin_data) * 8))[2:].zfill(22))
+    print((len(bin_data) * 8))
 
     # Insert dimensions
     bin_data.insert(0, height)
@@ -40,7 +35,6 @@ def ascii_to_bin(data):
 
 def hide_message(message, image):
     bin_msg = char_to_bin(message)
-    print(bin_msg)
 
     # Number of bits to be inserted
     bits = int(bin_msg[1], 2)
@@ -54,13 +48,10 @@ def hide_message(message, image):
         print(e.strerror)
     else:
 
-        # ascii to bin
         bin_data = [bin(int(x))[2:].zfill(8) for x in data]
 
-        print(len(bin_data), bits)
-
         if len(bin_data) < bits:
-            raise Exception("Message to big for carrier.")
+            raise ValueError()
 
         # Inserting Message with LSB technique
         y = 0
@@ -97,14 +88,10 @@ def retrieve_message(image):
         for x in range(8):
             bit_length += bin_data[x][-1]
 
-        print(int(bit_length, 2))
-
         message_length = ""
         # Retrieve Message Length (Bits Qty)
         for x in range(8, 8 + int(bit_length, 2)):
             message_length += bin_data[x][-1]
-
-        print(int(message_length, 2))
 
         bin_msg = []
         # Retrieve Message
@@ -112,7 +99,7 @@ def retrieve_message(image):
         acc = ""
         x = 8 + int(bit_length, 2)
         y = x + int(message_length, 2)
-        print(x, y)
+
         while x < y:
             if ctr < 8:
                 acc += bin_data[x][-1]
@@ -128,113 +115,114 @@ def retrieve_message(image):
         for x in bin_msg:
             message += chr(int(x, 2))
 
-
         return message
 
 
 def hide_image(secret, carrier):
 
-    with open(secret, "r") as a, open(carrier, "r") as b:
-        secret_data = [y for x in a.readlines()[1:] for y in x.replace("\n", "").split(" ") if y != ""]
+    try:
+        with open(secret, "r") as a, open(carrier, "r") as b:
 
-        carrier_header = b.readlines()[0:3]
-        b.seek(0)
-        carrier_data = [y for x in b.readlines()[3:] for y in x.replace("\n", "").split(" ") if y != ""]
+            secret_data = [y for x in a.readlines()[1:] for y in x.replace("\n", "").split(" ") if y != ""]
 
-    bin_secret_data = ascii_to_bin(secret_data)
-    print('bits to be inserted ', len("".join(bin_secret_data)))
-    print('bytes in carrier ', len(carrier_data))
-    if len("".join(bin_secret_data)) > int(len(carrier_data)):
-        raise Exception("Image is to big for carrier")
+            carrier_header = b.readlines()[0:3]
+            b.seek(0)
+            carrier_data = [y for x in b.readlines()[3:] for y in x.replace("\n", "").split(" ") if y != ""]
 
-    bin_carrrier_data = [bin(int(carrier_data[x]))[2:].zfill(8) for x in range(len("".join(bin_secret_data)))]
+    except FileNotFoundError:
+        print(FileNotFoundError.strerror)
+    else:
 
-    # Insert Dimensions
-    y = 0
-    for x in "".join(bin_secret_data)[0:32]:
-        while y < 32:
-            bin_carrrier_data[y] = bin_carrrier_data[y][0:len(bin_carrrier_data[y]) - 1] + x
-            y += 1
-            break
+        bin_carrier_data = [bin(int(x))[2:].zfill(8) for x in carrier_data]
+        bin_secret_data = "".join(ascii_to_bin(secret_data))
 
-    # Insert secret image into carrier image
-    j = 32
-    for x in "".join(bin_secret_data)[32:]:
-        while j < (len(bin_carrrier_data[32:])):
-            bin_carrrier_data[j] = bin_carrrier_data[j][0:len(bin_carrrier_data[j]) - 1] + x
-            j += 1
-            break
+        if len(bin_secret_data) > len(bin_carrier_data):
+            raise ValueError()
 
-    # Convert carrier image back to ascii
-    for x in range(len(bin_carrrier_data)):
-        carrier_data[x] = str(int(bin_carrrier_data[x], 2))
+        # Insert image into carrier
+        y = 0
+        bit_pos = 1
+        for x in bin_secret_data:
+            if y < len(bin_carrier_data):
+                if bit_pos > 1:
+                    bin_carrier_data[y] = bin_carrier_data[y][0:len(bin_carrier_data[y]) - bit_pos] + \
+                                          x + \
+                                          bin_carrier_data[y][-bit_pos + 1:]
+                    y += 1
+                else:
+                    bin_carrier_data[y] = bin_carrier_data[y][0:len(bin_carrier_data[y]) - bit_pos] + x
+                    y += 1
+            else:
+                y = 0
+                bit_pos += 1
+                bin_carrier_data[y] = bin_carrier_data[y][0:len(bin_carrier_data[y]) - bit_pos] + x + bin_carrier_data[y][-bit_pos + 1:]
+                y += 1
 
-    with open("stego_" + carrier, "w") as f:
-        f.write("".join(carrier_header) + "\n".join(carrier_data))
+        for x in range(len(bin_carrier_data)):
+            carrier_data[x] = str(int(bin_carrier_data[x], 2))
+
+        with open('steg_' + carrier, 'w') as f:
+            f.write("".join(carrier_header) + "\n".join(carrier_data))
 
 
 def retrieve_image(image):
 
-    with open(image, 'r') as f:
-        header_bin = [bin(int(y))[2:].zfill(8) for x in f.readlines()[3:35] for y in x.replace("\n", "").split(" ") if y != ""]
-        f.seek(0)
-        data_bin = [bin(int(y))[2:].zfill(8) for x in f.readlines()[35:] for y in x.replace("\n", "").split(" ") if y != ""]
+    try:
+        with open(image, 'r') as f:
 
-    # Extract header
-    h = ''
-    for x in header_bin:
-        h += x[-1]
+            header_bin = [bin(int(y))[2:].zfill(16) for x in f.readlines()[3:35] for y in x.replace("\n", "").split(" ") if y != ""]
+            f.seek(0)
+            l = [bin(int(y))[2:].zfill(16) for x in f.readlines()[35:57] for y in x.replace("\n", "").split(" ") if y != ""]
+            f.seek(0)
+            data_bin = [bin(int(y))[2:].zfill(16) for x in f.readlines()[57:] for y in x.replace("\n", "").split(" ") if y != ""]
 
-    print(h)
-    header = []
-    header.insert(0, str(int(h[0:16], 2)) + '\n')
-    header.insert(0, str(int(h[16:], 2)) + " ")
-    header.insert(0, _magic_number + "\n")
+    except FileNotFoundError:
+        print(FileNotFoundError.strerror)
+    else:
 
-    len_bit = ''
-    for x in range(8):
-        len_bit += data_bin[x][-1]
+        # Extract header
+        h = ''
+        for x in range(32):
+            h += header_bin[x][-1]
 
-    qty = ''
-    for x in range(int(len_bit, 2), 16):
-        qty += data_bin[x][-1]
+        header = _magic_number + "\n" + str(int(h[0:16], 2)) + " " + str(int(h[16:33], 2)) + "\n"
 
-    print(int(qty, 2))
+        #Extract data
+        message_length = ''
+        for x in range(len(l)):
+            message_length += l[x][-1]
 
-    # Extract data
-    data = []
-    acc = ''
-    ctr = 0
-    x = int(len_bit, 2) + len(qty)
-    y = x + int(qty, 2)
-    while x < y:
-        if ctr < 8:
-            acc += data_bin[x][-1]
-            ctr += 1
-            x += 1
-        else:
-            ctr = 0
-            data.append(acc)
-            acc = ''
-    data.append(acc)
+        message_length = int(message_length, 2)
 
-    # bin to ascii
-    for x in range(len(data)):
-        data[x] = str(int(data[x], 2))
+        d = ''
+        y = 0
+        bit_pos = -1
+        for x in range(message_length):
+            if y < len(data_bin):
+                d += data_bin[y][bit_pos]
+                y += 1
+            else:
+                y = 0
+                bit_pos -= 1
+                d += data_bin[y][bit_pos]
+                y += 1
 
-    with open("ret_image.ppm", 'w') as f:
-        f.write("".join(header) + "\n".join(data))
+        data = []
+        acc = ''
+        y = 0
+        for x in d:
+            if y < 8:
+                acc += x
+                y += 1
+            else:
+                data.append(str(int(acc, 2)))
+                y = 0
+                acc = ''
+                acc += x
+                y += 1
 
+        data.append(str(int(acc, 2)))
+        data.append("")
 
-if __name__ == '__main__':
-
-    # hide_message("Hola me llamo lastier de lasssllklllllllll pls lloiju jugjb juuh"
-    #              "falkjdlkajeiieiefjfoefeee"
-    #              "eeefefefwfqefqfqfqfqfq"
-    #              "qfqefqwefqwefqewfqe $%^ eioqeiuroroo\nkeieieieeiqoeij;adflkqjeflkj"
-    #              "ad;lfkja;lkdfj;lakjdf;lakjfa"
-    #              "alkdjl;fkjaldfkjd", "castillo.ppm")
-    # m = retrieve_message("stego_castillo.ppm")
-    # print(m)
-    hide_image("test.ppm", "castillo.ppm")
-    retrieve_image("stego_castillo.ppm")
+        with open("ret_image.ppm", 'w') as f:
+            f.write("".join(header) + "\n".join(data))
